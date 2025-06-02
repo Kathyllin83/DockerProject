b84fef4a6763223900b4f158e10c45a20e32acad
David-Evangelist <dev.davidwilliam@gmail.com>
2025-06-02 04:09:25 -0300

    chore: altera a porta do localhost

--- meu-projeto/backend/index.js
+++ meu-projeto/backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const Usuario = require("./models/usuario");
 
 const app = express();
-const PORT = 4000;
+const PORT = 3001;
 
 const MONGO_URI = process.env.MONGO_URI;
 
